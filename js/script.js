e50b28e760c5d70480925d3ae77f4d3a66c0b3ba
mahemoud2000 <140842379+mahemoud2000@users.noreply.github.com>
2024-04-22 01:29:06 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function white() {
+  document.body.style.background = "white";
+};
+function black() {
+  document.body.style.background = "black";
+};
+function gradient() {
+    document.body.style.background = "linear-gradient(to top, #ffffff, #000000)";
+  }
