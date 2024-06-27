6e069d2672e57ff7d8591c8f87eb9abe2d9140a9
mahemoud2000 <140842379+mahemoud2000@users.noreply.github.com>
2024-06-27 03:11:08 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 function white() {
   document.body.style.background = "white";
+  document.getElementById('contact').style.color = 'black';
+  document.getElementById('number').style.color = 'black';
+
 };
 function black() {
   document.body.style.background = "black";
+  document.getElementById('contact').style.color = 'white';
+  document.getElementById('social').style.backgroundColor = 'white';
+  document.getElementById('social-2').style.backgroundColor = 'white';
+  document.getElementById('social-links').style.color = 'white';
+  document.getElementById('call-links').style.color = 'white';
+  document.getElementById('number').style.color = 'black';
+
 };
 function gradient() {
     document.body.style.background = "linear-gradient(to top, #ffffff, #000000)";
-  }
+};
