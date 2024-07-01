df2d2ba3e70a49f37dea1179fa923e2152678f1a
mahemoud2000 <140842379+mahemoud2000@users.noreply.github.com>
2024-07-01 04:35:32 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
+let colorWhite = "white";
+let colorBlack = "black";
 function white() {
   document.body.style.background = "white";
-  document.getElementById('contact').style.color = 'black';
-  document.getElementById('number').style.color = 'black';
-
+  document.getElementById('f').style.color = colorBlack;
+  document.getElementById('t').style.color = colorBlack;
+  document.getElementById('l').style.color = colorBlack;
+  document.getElementById('m').style.color = colorBlack;
+  document.getElementById('p').style.color = colorBlack;
+  document.getElementById('w').style.color = colorBlack;
+  document.getElementById('bc-social').style.backgroundColor = "#6860607a";
+  document.getElementById('bc-contact').style.backgroundColor = "#6860607a";
+  // document.getElementById('social').style.backgroundColor = "#6860607a";
+  document.getElementById('social').style.boxShadow = '1px 1px 10px black,-1px -1px 10px black';
+  document.getElementById('form').style.boxShadow = '1px 1px 10px black,-1px -1px 10px black';
 };
 function black() {
   document.body.style.background = "black";
-  document.getElementById('contact').style.color = 'white';
-  document.getElementById('social').style.backgroundColor = 'white';
-  document.getElementById('social-2').style.backgroundColor = 'white';
-  document.getElementById('social-links').style.color = 'white';
-  document.getElementById('call-links').style.color = 'white';
-  document.getElementById('number').style.color = 'black';
+  document.getElementById('f').style.color = colorWhite;
+  document.getElementById('t').style.color = colorWhite;
+  document.getElementById('l').style.color = colorWhite;
+  document.getElementById('m').style.color = colorWhite;
+  document.getElementById('p').style.color = colorWhite;
+  document.getElementById('w').style.color = colorWhite;
+  document.getElementById('social').style.boxShadow = '1px 1px 10px white,-1px -1px 10px white';
+  document.getElementById('form').style.boxShadow = '1px 1px 10px white,-1px -1px 10px white';
+  document.getElementById('bc-social').style.backgroundColor = colorBlack;
+  document.getElementById('bc-contact').style.backgroundColor = colorBlack;
 
 };
 function gradient() {
     document.body.style.background = "linear-gradient(to top, #ffffff, #000000)";
 };
+
+
