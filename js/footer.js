ec8c53e6623c928e713f22b966aceea03bff4d55
mahemoud2000 <140842379+mahemoud2000@users.noreply.github.com>
2024-07-06 04:47:01 +0300

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,27 @@
 let footer = `
-        <p>&copy;2024 By Mahmoud Hamed</p>
+       <div class="main-footer">
+       <ul class="contact-us">
+           <li><a href="#">facebook</a></li>
+           <li><a href="#">Tiktok</a></li>
+           <li><a href="#">Lenkid In</a></li>
+           <li><a href="#">mail</a></li>
+           <li><a href="#">whatsapp</a></li>
+           <li><a href="#">My Phone</a></li>
+       </ul>
+       <div class="logo-footer">
+           <a href="#"><img src="images/logo-white.png" alt=""></a>
+           <h3>Mahmoud Hamed</h3>
+       </div>
+        <ul class="contact-us">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Skills</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Call Me</a></li>
+        </ul>
+    </div>
+    <p>Made By Mahmoud Hamed &copy;2024</p>
 
 `;
 document.getElementById('footer').innerHTML = footer;
