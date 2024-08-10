851f949e670b0f647d3227520e7b0af3b5d347e2
mahemoud2000 <140842379+mahemoud2000@users.noreply.github.com>
2024-08-11 02:33:51 +0300

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,37 +1,37 @@
 let heading = `<a href="#" class="btn-project">view</a>`;
 let projects =[`
     <div class="project">
-      ${heading}
+      <a href="https://al-hamed-group.github.io/ALhamed/index.html" class="btn-project">view</a>
       <img src="images/logo.png" alt="">
     </div>`,
     `
     <div class="project"> 
-    ${heading}
+   <a href="https://mahemoud2000.github.io/Food-/" class="btn-project">view</a>
     <img src="images/food.jpg" alt="">
     </div>`,
     `
     <div class="project">
-    ${heading}
+   <a href="https://p-fresh.github.io/P-fresh/" class="btn-project">view</a>
     <img src="images/P-fresh.png" alt="">
     </div>`,
     `
     <div class="project">
-      ${heading}
+      <a href="https://mahemoud2000.github.io/Web/" class="btn-project">view</a>
       <img src="images/learn-alhamed.jpeg" alt="">
     </div>`,
     `
       <div class="project">
-      ${heading}
+     <a href="https://broccolistore.github.io/broccolii/" class="btn-project">view</a>
       <img src="images/logo-broocli.WebP" alt="">
     </div>`,
     `
     <div class="project">
-    ${heading}
+   <a href="#" class="btn-project">view</a>
     <img src="images/protfolio.png" alt="">
     </div>`,
     `
       <div class="project">
-      ${heading}
+     <a href="#" class="btn-project">view</a>
       <img src="images/project-3.png" alt="">
     </div>`,
     `
