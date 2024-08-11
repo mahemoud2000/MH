68e09c3d3a7e70b0d0ac2d9dad9ae6679fe2e204
mahemoud2000 <140842379+mahemoud2000@users.noreply.github.com>
2024-08-11 23:34:18 +0300

    Add files via upload

---
+++ js/topskills.js
@@ -0,0 +1,19 @@
+
+let topSkills = `
+ <h2>Top Skills</h2>
+                    <div class="all-top-skills">
+                        <div class="html">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST4fX7exNZgcbevxUjYop2_qq4uZgTlZ0rvA&s" alt="">
+                        </div>
+                        <div class="css">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7mBOiYwVH_eCcZ2LC81SFFvLJ9OkAYEL2_A&s" alt="">
+                        </div>
+                        <div class="js">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgh0cmaLPqUaZd6iuo7D_iaOeLYb1QUdjMiA&s" alt="">
+                        </div>
+                    </div>
+`;
+
+
+
+ document.getElementById('top-skills').innerHTML = topSkills;
